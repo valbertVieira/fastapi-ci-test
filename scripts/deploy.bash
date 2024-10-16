268b91e2d0f35aef865ead68402eb0334468cb03
valbertVieira <142999483+valbertVieira@users.noreply.github.com>
2024-10-16 16:43:58 -0300

    Update deploy.bash

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -60,8 +60,8 @@ Restart=always
 RestartSec=1
 User=root
 WorkingDirectory=$REMOTE_PATH
-ExecStartPre=/bin/bash -c 'source /root/.bashrc && poetry --version'
-ExecStart=/root/.local/bin/poetry run uvicorn main:app --host 0.0.0.0 --port 8080
+ExecStartPre=/root/.local/bin/poetry install
+ExecStart=/root/.local/bin/poetry run uvicorn main:app --host 0.0.0.0 --port ${API_PORT}
 [Install]
 WantedBy=multi-user.target
 SERVICE_EOF
