f8dbca73dfa05e5b0c253508466edee6817aa6ca
valbertVieira <142999483+valbertVieira@users.noreply.github.com>
2024-10-17 15:01:39 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-# @app.get('/')
-# async def read_results():
-#     results = {"v":"2.1"}
-#     return results
+@app.get('/')
+async def read_results():
+    results = {"v":"2.2"}
+    return results
 
 
 #with same problem
