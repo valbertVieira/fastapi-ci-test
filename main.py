cff195d7b1c6420783d99d3ff9af0b5564f09f06
valbertVieira <142999483+valbertVieira@users.noreply.github.com>
2024-10-17 14:55:59 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-async def read_results():
-    results = {"v":"2.1"}
-    return results
+# @app.get('/')
+# async def read_results():
+#     results = {"v":"2.1"}
+#     return results
 
 
